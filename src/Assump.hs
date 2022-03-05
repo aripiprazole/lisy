6f48dc5850e8922a248c19bf8bfbce75aa63cbe7
Gabrielle Guimarães <gabrielle1guim@gmail.com>
2022-03-05 07:47:06 -0300

    add assumption

---
+++ src/Assump.hs
@@ -0,0 +1,20 @@
+module Assump (Assump (..), find) where
+
+import Ast (Name)
+import Control.Monad (MonadFail)
+import Scheme (Scheme)
+import Types (Types (apply, ftv))
+
+-- | Assumption about a type variable.
+data Assump = Name :>: Scheme
+
+instance Types Assump where
+  apply s (n :>: sc) = n :>: apply s sc
+  ftv (_ :>: sc) = ftv sc
+
+-- | Finds a scheme given a assumption context.
+find :: MonadFail m => Name -> [Assump] -> m Scheme
+find n [] = fail $ "unbound name: " ++ show n
+find n ((n' :>: sc) : as)
+  | n == n' = return sc
+  | otherwise = find n as
