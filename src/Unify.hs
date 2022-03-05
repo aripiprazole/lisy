b644a4dbfdcd8485d98e80ef09f965f19431dc7f
Gabrielle Guimarães <gabrielle1guim@gmail.com>
2022-03-05 02:15:46 -0300

    add unification functions

---
+++ src/Unify.hs
@@ -0,0 +1,40 @@
+module Unify where
+
+import Ast (HasKind (kind), TyVar, Typ (TApp, TCon, TVar))
+import Types (Subst, Types (ftv), merge, nullSubst, (+->), (@@))
+
+-- | `mgu`, that stand for most geral unifier, unifies two types and returns
+-- a substition s that `apply s t1` is equal to `apply s t2`, if can't unify
+-- it fails
+mgu :: MonadFail m => Typ -> Typ -> m Subst
+mgu (TApp a b) (TApp a' b') = do
+  s1 <- mgu a a'
+  s2 <- mgu b b'
+  return $ s2 @@ s1
+mgu (TVar u) t = varBind u t
+mgu t (TVar u) = varBind u t
+mgu (TCon tc1) (TCon tc2) | tc1 == tc2 = return nullSubst
+mgu _ _ = fail "types do not unify"
+
+-- | Unifies type variable to a type,
+-- if `u` is not in free variables of `t`,
+-- if kind of `u` is equal to kind of `t`, otherwise it fails.
+varBind :: MonadFail m => TyVar -> Typ -> m Subst
+varBind u t
+  | t == TVar u = return nullSubst
+  | u `elem` ftv t = fail "occurs check failed"
+  | kind u /= kind t = fail "kinds do not match"
+  | otherwise = return $ u +-> t
+
+-- | Combines two types like `mgu`, but the goal is find a substitution s that
+-- `apply s t1` is equal to `t2`, this operation is frequently called one-way matching.
+-- It uses `merge` rather than `@@` to compose substituions.
+-- If cant unify it fails.
+match :: MonadFail m => Typ -> Typ -> m Subst
+match (TApp a b) (TApp a' b') = do
+  s1 <- match a a'
+  s2 <- match b b'
+  merge s2 s1
+match (TVar u) t | kind u == kind t = return $ u +-> t
+match (TCon tc1) (TCon tc2) | tc1 == tc2 = return nullSubst
+match _ _ = fail "types do not match"
