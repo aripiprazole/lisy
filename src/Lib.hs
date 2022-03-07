e8daa2421b438dc1bd4c8b08f0692eb74271e8d6
Gabrielle Guimarães <gabrielle1guim@gmail.com>
2022-03-06 21:44:41 -0300

    test adhoc

--- src/Lib.hs
+++ src/Lib.hs
@@ -3,44 +3,54 @@ module Lib
     sample,
     std,
     toBg,
-    programEnv,
   )
 where
 
-import Adhoc (ClassEnv (ClassEnv), Pred (IsIn), Qual ((:=>)), addPreludeClasses, initialEnv, (<:>))
+import Adhoc (ClassEnv (ClassEnv), Pred (IsIn), Qual ((:=>)), addInst, addPreludeClasses, initialEnv, (<:>))
 import Assump (Assump ((:>:)))
-import Ast (Alt (Alt), BindGroup (BindGroup), Exp (ELit), Expl (Expl), Impl (Impl), Lit (LString, LUnit), Pat (PVar))
+import Ast (Alt (Alt), BindGroup (BindGroup), Exp (EApp, ELit, EVar), Expl (Expl), Impl (Impl), Lit (LString, LUnit), Pat (PVar))
 import Data.Maybe (fromJust)
-import Infer (tiProgram)
+import Entailment (entail)
+import Infer (tiExp, tiProgram)
 import Name (Name (Id))
 import Reduction (simplify)
 import Scheme (Scheme (Forall), toScheme)
-import TI (Instantiate (inst))
-import Types (Kind (KFun, KStar), TyVar (TyVar), Typ (TGen, TVar), list, tInt, tString, tUnit, (->>))
+import TI (Instantiate (inst), getSubst, runTI)
+import Types (Kind (KFun, KStar), TyVar (TyVar), Typ (TGen, TVar), Types (apply), list, tInt, tString, tUnit, (->>))
 
-programEnv :: IO ClassEnv
-programEnv = addPreludeClasses initialEnv
+atyp = TVar (TyVar (Id "a") KStar)
 
 std :: [Assump]
-std = [Id "println" :>: Forall [] ([] :=> (tString ->> tUnit))]
+std =
+  [ Id "println" :>: Forall [] ([] :=> (tString ->> tUnit)),
+    Id "show" :>: Forall [] ([IsIn atyp (Id "Show")] :=> (atyp ->> tString))
+  ]
 
 sample :: (Name, Maybe Scheme, [Alt])
-sample = (Id "main", Just $ Forall [] ([IsIn tInt (Id "Num")] :=> (list tString ->> tUnit)), [Alt [PVar $ Id "args"] $ ELit LUnit])
+sample =
+  ( Id "main",
+    Just $ Forall [] ([] :=> (list tString ->> tString)),
+    [Alt [PVar $ Id "args"] $ EApp (EVar (Id "show")) (ELit LUnit)]
+  )
 
 toBg :: [(Name, Maybe Scheme, [Alt])] -> BindGroup
 toBg g =
   BindGroup [Expl v t alts | (v, Just t, alts) <- g] $
     filter (not . null) [[Impl v alts | (v, Nothing, alts) <- g]]
 
+env = [Id "args" :>: Forall [] ([] :=> list tString)]
+
 someFunc :: IO ()
 someFunc = do
-  pe <- programEnv
-  let pg = tiProgram pe std [toBg [sample]]
-  let a = TVar (TyVar (Id "a") KStar)
-  let b = TVar (TyVar (Id "b") KStar)
+  ce' <- addPreludeClasses initialEnv
+  ce <- addInst [] (IsIn (list tString) (Id "Show")) ce'
 
-  print (toBg [sample])
-  print $ simplify pe [IsIn tInt (Id "Num")]
-  print std
-  print pg
+  print $ tiProgram ce std [toBg [sample]]
+  let (ps, t) = runTI $ do
+        (qs, t) <- tiExp ce (std ++ env) (EApp (EVar $ Id "show") (EVar $ Id "args"))
+        s <- getSubst
+
+        return (apply s qs, apply s t)
+  print (ps, t)
+  print $ entail ce [] (head ps)
   return ()
